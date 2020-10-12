c1e12443d253aedef473866dc582d011536331bb
Marc 'risson' Schmitt <risson@cri.epita.fr>
2020-10-12 22:14:41 +0200

    pkgs: add sddm-epita-themes
    Signed-off-by: Marc 'risson' Schmitt <risson@cri.epita.fr>

---
+++ pkgs/applications/display-managers/sddm/sddm-epita-themes.nix
@@ -0,0 +1,23 @@
+{ stdenv, fetchurl, extraThemeConfig ? "" }:
+
+stdenv.mkDerivation {
+  pname = "sddm-epita-themes";
+  version = "1.0-2";
+
+  src = fetchurl {
+    url = "http://static.cri.epita.net/pkg/epita-themes-sddm.tgz";
+    sha256 = "0icgbrzh9n5g8y6f3hhyianh3rw2i8igcawp6djzh6kv05fb3k1h";
+  };
+
+  unpackPhase = ''
+    tar xf $src
+  '';
+
+  installPhase = ''
+    install -d $out/share/sddm/themes/epita-simplyblack
+    install -Dm644 \
+      epita-simplyblack/* \
+      $out/share/sddm/themes/epita-simplyblack
+    echo "${extraThemeConfig}" >> $out/share/sddm/themes/epita-simplyblack/theme.conf
+  '';
+}
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,3 +1,4 @@
 _: prev: {
   term_size = prev.callPackage ./tools/misc/term_size { };
+  sddm-epita-themes = prev.callPackage ./applications/display-managers/sddm/sddm-epita-themes.nix { };
 }
