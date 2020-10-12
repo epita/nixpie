42e3df16cf81dd5a1cbf8e8a56172f916ea0635a
Marc 'risson' Schmitt <risson@cri.epita.fr>
2020-10-12 22:20:35 +0200

    modules: add aria2
    Signed-off-by: Marc 'risson' Schmitt <risson@cri.epita.fr>

--- modules/list.nix
+++ modules/list.nix
@@ -3,6 +3,7 @@
 
   ./services/audio/alsa.nix
   ./services/hardware/bluetooth.nix
+  ./services/networking/aria2.nix
   ./services/networking/sshd/sshd.nix
 
   ./system/boot/netboot.nix
---
+++ modules/services/networking/aria2.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+{
+  options = {
+    cri.aria2 = {
+      enable = mkEnableOption "Whether to enable aria2.";
+      seedlist = mkOption {
+        default = "${config.netboot.torrent.mountPoint}/aria2_seedlist.txt";
+        description = "aria2 seedlist file to use for seeding torrent.";
+      };
+    };
+  };
+
+  config = mkIf config.cri.aria2.enable {
+    networking.firewall = {
+      allowedUDPPortRanges = [{ from = 6881; to = 6999; }];
+    };
+
+    systemd.services.aria2 = {
+      description = "aria2";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        Type = "simple";
+      };
+
+      script = ''
+        ${pkgs.aria2}/bin/aria2c --enable-dht=false --seed-ratio=0 -V -i ${config.cri.aria2.seedlist}
+      '';
+    };
+  };
+}
