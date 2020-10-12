6fd3e0a83112dc8aedfb3fd124ed49b1a685f124
Marc 'risson' Schmitt <risson@cri.epita.fr>
2020-10-12 22:19:06 +0200

    modules: add yubikey
    Signed-off-by: Marc 'risson' Schmitt <risson@cri.epita.fr>

--- modules/list.nix
+++ modules/list.nix
@@ -1,4 +1,6 @@
 [
+  ./programs/yubikey.nix
+
   ./services/networking/sshd/sshd.nix
 
   ./system/boot/netboot.nix
---
+++ modules/programs/yubikey.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+{
+  options = {
+    cri.yubikey = {
+      enable = mkEnableOption "Enable yubikey-related tools";
+    };
+  };
+
+  config = mkIf config.cri.yubikey.enable {
+    services.pcscd.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      yubikey-manager
+      yubikey-personalization
+      yubioath-desktop
+    ];
+  };
+}
