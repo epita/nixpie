f2b8bbf6dc6e1d3922cc4ec17cc337c03125c80a
Marc 'risson' Schmitt <risson@cri.epita.fr>
2021-06-15 21:19:26 +0200

    images/nixos-sup: add required packages
    Signed-off-by: Marc 'risson' Schmitt <risson@cri.epita.fr>

--- images/nixos-sup.nix
+++ images/nixos-sup.nix
@@ -1,7 +1,17 @@
+{ config, pkgs, ... }:
+
+let
+  nixosSupPkgs = with pkgs; [
+    gnome.gedit
+    gource
+  ];
+in
 {
   imports = [
     ../profiles/graphical
   ];
+
+  environment.systemPackages = with config.cri.programs; devOcaml ++ devCsharp ++ nixosSupPkgs;
 
   netboot.enable = true;
   cri.sddm.title = "NixOS SUP";
