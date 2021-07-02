d53b564bfb6eca3fdb463cd86f11e04039a49610
Marc 'risson' Schmitt <risson@cri.epita.fr>
2021-07-02 14:08:05 +0200

    Revert "pkgs: sddm-epita-themes: follow naming conventions"
    This reverts commit 4573db5edf008846db6332239e4016a16801b20b.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,5 +3,5 @@ final: prev:
 {
   libfff = final.callPackage ./development/libraries/libfff { };
   termSize = final.callPackage ./tools/misc/term-size { };
-  sddmEpitaThemes = final.callPackage ./applications/display-managers/sddm/sddm-epita-themes.nix { };
+  sddm-epita-themes = final.callPackage ./applications/display-managers/sddm/sddm-epita-themes.nix { };
 }
