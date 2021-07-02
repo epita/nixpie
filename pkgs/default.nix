bd58e5d12b914eebc1ce1697ff96d9140cf3f265
Marc 'risson' Schmitt <risson@cri.epita.fr>
2021-07-02 15:01:14 +0200

    pkgs: add exec-tools
    Signed-off-by: Marc 'risson' Schmitt <risson@cri.epita.fr>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,7 @@
 final: prev:
 
 {
+  exec-tools = final.callPackage ./exec-tools { };
   libfff = final.callPackage ./development/libraries/libfff { };
   term_size = final.callPackage ./tools/misc/term_size { };
   sddm-epita-themes = final.callPackage ./applications/display-managers/sddm/sddm-epita-themes.nix { };
