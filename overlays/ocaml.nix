514eae64a4169e9c0447eb6c379e1a794f48c5fc
Marc 'risson' Schmitt <risson@cri.epita.fr>
2021-09-05 23:29:29 +0200

    overlays: ocaml: rework overlay to make a proper use of wrapProgram
    Signed-off-by: Marc 'risson' Schmitt <risson@cri.epita.fr>

--- overlays/ocaml.nix
+++ overlays/ocaml.nix
@@ -1,14 +1,13 @@
 final: prev: {
-  ocaml = final.stdenv.mkDerivation rec {
-    name = prev.ocaml.name;
+  ocaml = final.symlinkJoin rec {
+    inherit (prev.ocaml) name;
     version = final.lib.getVersion prev.ocaml;
-    nativeBuildInputs = [ final.makeWrapper ];
 
-    buildCommand = ''
-      mkdir -p $out/bin
-      cp ${prev.ocaml}/bin/* $out/bin/
-      rm "$out/bin/ocaml"
-      makeWrapper "${prev.ocaml}/bin/ocaml" "$out/bin/ocaml" \
+    paths = [ prev.ocaml ];
+    buildInputs = [ final.makeWrapper ];
+
+    postBuild = ''
+      wrapProgram $out/bin/ocaml \
         --add-flags "-I ${final.ocamlPackages.findlib}/lib/ocaml/${version}/site-lib"
     '';
   };
