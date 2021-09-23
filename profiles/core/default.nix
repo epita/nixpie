7714a27c1fd3dba120842f661f624fabee138ce7
Marc 'risson' Schmitt <risson@cri.epita.fr>
2021-09-23 16:35:51 +0200

    profiles: core: set proper options to be able to link external libs
    Signed-off-by: Marc 'risson' Schmitt <risson@cri.epita.fr>

--- profiles/core/default.nix
+++ profiles/core/default.nix
@@ -85,6 +85,7 @@ with lib;
   environment.extraOutputsToInstall = [ "out" "lib" "bin" "dev" ];
   environment.variables = {
     NIX_CFLAGS_COMPILE_x86_64_unknown_linux_gnu = "-I/run/current-system/sw/include";
+    NIX_CFLAGS_LINK_x86_64_unknown_linux_gnu = "-L/run/current-system/sw/lib";
     PKG_CONFIG_PATH = "/run/current-system/sw/lib/pkgconfig";
   };
 
