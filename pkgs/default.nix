9162dae37608d9d7b594f3aec8badae5afcfec3a
Marc 'risson' Schmitt <risson@cri.epita.fr>
2021-09-26 02:45:39 +0200

    pkgs: add nixpie-utils
    Signed-off-by: Marc 'risson' Schmitt <risson@cri.epita.fr>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,6 +8,7 @@ final: prev:
   intel_nuc_led = final.callPackage ./os-specific/linux/intel_nuc_led { inherit (final.linuxPackages) kernel; };
   libfff = final.callPackage ./development/libraries/libfff { };
   m68k = final.qt5.callPackage ./development/tools/m68k { };
+  nixpie-utils = final.callPackage ./nixpie-utils { };
   pam_afs_session = final.callPackage ./os-specific/linux/pam_afs_session { };
   term_size = final.callPackage ./tools/misc/term_size { };
   sddm-epita-themes = final.callPackage ./applications/display-managers/sddm/sddm-epita-themes.nix { };
---
+++ pkgs/nixpie-utils/default.nix
@@ -0,0 +1,56 @@
+{ lib
+, symlinkJoin
+, writeShellScriptBin
+, coreutils
+, gawk
+, gnugrep
+, inetutils
+, iproute2
+}:
+
+let
+  wait_for_hostname = ''
+    n=0
+    until [ "''${n}" -ge 15 ] || [ "${inetutils}/bin/hostname" != "localhost" ]; do
+      n="$(( ''${n} + 1 ))"
+      sleep 2
+    done
+  '';
+  get_ip = writeShellScriptBin "get_ip.sh" ''
+    while true; do
+      ip="$(${iproute2}/bin/ip address \
+        | ${gnugrep}/bin/grep 'inet ' \
+        | ${gnugrep}/bin/grep -v '127.0.0.' \
+        | ${coreutils}/bin/head -n1 \
+        | ${gawk}/bin/awk '{ print $2 }' \
+        | ${coreutils}/bin/cut -d/ -f1\
+      )"
+      if [ -n "$ip" ] ; then
+        break
+      fi
+      sleep 2
+    done
+    echo "''${ip}"
+  '';
+  get_room_name = writeShellScriptBin "get_room_name.sh" ''
+    ${wait_for_hostname}
+    hostname -f | sed 's/.sm.cri.epita.fr//' | cut -d. -f2
+  '';
+  get_site_name = writeShellScriptBin "get_site_name.sh" ''
+    ${wait_for_hostname}
+    hostname -f | sed 's/.sm.cri.epita.fr//' | cut -d. -f3
+  '';
+in
+symlinkJoin {
+  name = "nixpie-utils";
+
+  paths = [
+    get_ip
+    get_room_name
+    get_site_name
+  ];
+
+  meta = with lib; {
+    platforms = platforms.linux;
+  };
+}
