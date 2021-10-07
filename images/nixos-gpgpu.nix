294f2a946e66e5ceed93411c09867af658ae1dd2
Marc 'risson' Schmitt <risson@cri.epita.fr>
2021-10-07 15:29:22 +0200

    nixos-gpgpu: add opengl and gpgpu programs
    Signed-off-by: Marc 'risson' Schmitt <risson@cri.epita.fr>

--- images/nixos-gpgpu.nix
+++ images/nixos-gpgpu.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   imports = [
@@ -8,9 +8,22 @@
   netboot.enable = true;
   cri.sddm.title = "NixOS GPGPU";
 
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  boot.kernelParams = [
+    "NVreg_RestrictProfilingToAdminUsers=0"
+    "nomodeset"
+  ];
+
   cri.programs.packages = with config.cri.programs.packageBundles; [
     dev
+    gpgpu
+    opengl
   ];
 
   cri.programs.pythonPackages = with config.cri.programs.pythonPackageBundles; [ dev ];
+
+  environment.variables = {
+    CUDA_PATH = "${pkgs.cudatoolkit}";
+  };
 }
