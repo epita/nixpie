7a366728f7f38d9ca6b406ab6d9043e52e2a5137
Marc 'risson' Schmitt <risson@cri.epita.fr>
2021-10-08 22:49:27 +0200

    profiles: core: allow unfree packages
    Signed-off-by: Marc 'risson' Schmitt <risson@cri.epita.fr>

--- profiles/core/default.nix
+++ profiles/core/default.nix
@@ -87,6 +87,8 @@ with lib;
   environment.pathsToLink = [ "/include" "/lib" ];
   environment.extraOutputsToInstall = [ "out" "lib" "bin" "dev" ];
   environment.variables = {
+    NIXPKGS_ALLOW_UNFREE = "1";
+
     NIX_CFLAGS_COMPILE_x86_64_unknown_linux_gnu = "-I/run/current-system/sw/include";
     NIX_CFLAGS_LINK_x86_64_unknown_linux_gnu = "-L/run/current-system/sw/lib";
     PKG_CONFIG_PATH = "/run/current-system/sw/lib/pkgconfig";
