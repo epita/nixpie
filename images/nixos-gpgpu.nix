56444a6342739482611f8e418117071fcef48fcd
Marc 'risson' Schmitt <risson@cri.epita.fr>
2021-10-10 18:48:54 +0200

    nixos-gpgpu: add shell.nix for cudatoolkit
    Signed-off-by: Marc 'risson' Schmitt <risson@cri.epita.fr>

--- images/nixos-gpgpu.nix
+++ images/nixos-gpgpu.nix
@@ -35,4 +35,45 @@
   environment.variables = {
     CUDA_PATH = "${pkgs.cudatoolkit}";
   };
+
+  environment.etc."nixos-gpgpu/shell.nix".text = ''
+    let
+      system = "x86_64-linux";
+
+      nixpie = import <nixpie>;
+
+      inherit (nixpie.inputs.nixpkgs) lib;
+      inherit (lib) attrValues;
+
+      pkgs = import nixpie.inputs.nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+        overlays = (attrValues nixpie.overlays) ++ [ nixpie.overrides.''${system} ];
+      };
+    in
+    pkgs.stdenv.mkDerivation {
+      name = "cuda-env-shell";
+      buildInputs = with pkgs; [
+        git gitRepo gnupg autoconf curl
+        procps gnumake utillinux m4 gperf unzip cmake
+        linuxPackages.nvidia_x11
+        libGLU libGL
+        xorg.libXi xorg.libXmu freeglut
+        xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib pngpp tbb
+        ncurses5 stdenv.cc binutils
+        (cudatoolkit.override {
+          gcc = stdenv.cc;
+        })
+      ];
+      shellHook = '' + "''" + ''
+    export CUDA_PATH=''${pkgs.cudatoolkit}
+    export LD_LIBRARY_PATH=''${pkgs.linuxPackages.nvidia_x11}/lib:''${pkgs.ncurses5}/lib
+    export EXTRA_LDFLAGS="-L/lib -L''${pkgs.linuxPackages.nvidia_x11}/lib"
+    export EXTRA_CCFLAGS="-I/usr/include"
+    export PATH=$CUDA_PATH/nsight_compute:$PATH
+  '' + "''" + '';
+    }
+  '';
 }
