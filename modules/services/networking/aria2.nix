64436479b716987553dd3b8448e58254335cb890
Marin Hannache <mareo@cri.epita.fr>
2021-11-19 10:11:18 +0100

    modules: networking: disable aria2 seeding ratio limit
    Signed-off-by: Marin Hannache <mareo@cri.epita.fr>

--- modules/services/networking/aria2.nix
+++ modules/services/networking/aria2.nix
@@ -32,7 +32,8 @@ with lib;
         aria2_base="-V --file-allocation=prealloc --enable-mmap=true --bt-enable-lpd=true"
         aria2_summary="--summary-interval=60"
         aria2_nodht="--enable-dht=false --enable-dht6=false"
-        aria2_opts="$aria2_base $aria2_summary $aria2_nodht"
+        aria2_always_seed="--seed-ratio=0"
+        aria2_opts="$aria2_base $aria2_summary $aria2_nodht $aria2_always_seed"
 
         ${pkgs.aria2}/bin/aria2c $aria2_opts --check-integrity --input-file=${config.cri.aria2.seedlist}
       '';
