82fc75d3f95ed3eee641f9ae705391a916035892
Hugo Moreau <hugo.moreau@epita.fr>
2022-01-03 10:50:50 +0100

    nixos-pie: add devSQL

--- images/nixos-pie.nix
+++ images/nixos-pie.nix
@@ -8,7 +8,7 @@
   netboot.enable = true;
   cri.sddm.title = "NixOS PIE";
 
-  cri.programs.packages = with config.cri.programs.packageBundles; [ dev devThl ];
+  cri.programs.packages = with config.cri.programs.packageBundles; [ dev devSQL devThl ];
   cri.programs.pythonPackages = with config.cri.programs.pythonPackageBundles; [ dev devThl ];
 
   environment.systemPackages = with pkgs; [ ciscoPacketTracer8 ];
