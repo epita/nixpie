66b7d8025e986925733a7db95cbbf87481a2145d
Marin Hannache <mareo@cri.epita.fr>
2022-01-10 17:16:58 +0100

    tests: nswrappers: remove useless password input
    Signed-off-by: Marin Hannache <mareo@cri.epita.fr>

--- tests/nswrappers.nix
+++ tests/nswrappers.nix
@@ -7,7 +7,6 @@
     users.users.alice = {
       isNormalUser = true;
       group = "students";
-      password = "hunter2"; # *******
     };
     users.groups.students = {
       gid = 15000;
@@ -16,7 +15,7 @@
 
   testScript = ''
     start_all()
-    machine.succeed("su - alice -c 'echo hunter2 | sudo -S ns-init test-ns'")
+    machine.succeed("su - alice -c 'sudo ns-init test-ns'")
     machine.succeed("ip netns ls | grep test-ns")
   '';
 }
