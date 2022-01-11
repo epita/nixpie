40acf89cd2b7f10b990ffdbaed02d2cba6c928e7
Marc 'risson' Schmitt <risson@cri.epita.fr>
2022-01-11 14:39:34 +0100

    nixos-pie: add devJava
    Signed-off-by: Marc 'risson' Schmitt <risson@cri.epita.fr>

--- images/nixos-pie.nix
+++ images/nixos-pie.nix
@@ -8,8 +8,13 @@
   netboot.enable = true;
   cri.sddm.title = "NixOS PIE";
 
-  cri.programs.packages = with config.cri.programs.packageBundles; [ dev devSQL devThl ];
+  cri.programs.packages = with config.cri.programs.packageBundles; [ dev devSQL devThl devJava ];
   cri.programs.pythonPackages = with config.cri.programs.pythonPackageBundles; [ dev devThl ];
 
   cri.nswrappers.enable = true;
+
+  programs.java = {
+    enable = true;
+    package = pkgs.jdk;
+  };
 }
