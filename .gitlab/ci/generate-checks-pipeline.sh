9461bd015ca2f9d442677e3504e4976e26cef41e
Marc 'risson' Schmitt <risson@cri.epita.fr>
2022-01-11 13:58:57 +0100

    ci: always run checks
    Signed-off-by: Marc 'risson' Schmitt <risson@cri.epita.fr>

--- .gitlab/ci/generate-checks-pipeline.sh
+++ .gitlab/ci/generate-checks-pipeline.sh
@@ -11,60 +11,13 @@ mkdir -p "${CHECKS_DIFF_DIR}"
 
 print_defaults
 
-function getCheckDrvPath() {
-  repo="${1}"
-  check="${2}"
-
-  path="${repo}#checks.x86_64-linux.${check}.drvPath"
-
-  echo "${path}"
-}
-
-function didCheckChange() {
-  check="${1}"
-  diffFile="${CHECKS_DIFF_DIR}/${check}"
-  currentCheckDrvPath="$(getCheckDrvPath "${CI_PROJECT_DIR}" "${check}")"
-  previousCheckDrvPath="$(getCheckDrvPath "git+${CI_PROJECT_URL}?ref=${CI_MERGE_REQUEST_TARGET_BRANCH_NAME}" "${check}")"
-
-  currentDrv="$(nix eval --raw "${currentCheckDrvPath}")"
-  previousDrv="$(nix eval --raw "${previousCheckDrvPath}")" || return 0
-
-  # We allow 31 lines of differences, which is the amount that changes when
-  # only the commit SHA changes.
-  diffDrv "${previousDrv}" "${currentDrv}" "${diffFile}" 31
-}
-
 echoInfo "Listing all checks..."
 checks="$(nix_run list-checks | xargs)"
 echoInfo "Checks found: ${checks}"
 
-changedChecks=""
-
-echoInfo "Starting pipeline generation..."
-
-if [ -z "${CI_MERGE_REQUEST_IID:-}" ] || [ -n "${ALL_CHECKS:-}" ]; then
-  echoWarn "Pipeline is not attached to a merge request."
-  echoWarn "All checks will be rebuilt."
-  changedChecks="${checks}"
-else
-  echoWarn "Pipeline is attached to a merge request."
-  echoWarn "Checking what checks we should rebuild..."
-  for check in ${checks}; do
-    echoInfo "Checking if check ${check} changed..."
-    if didCheckChange "${check}"; then
-      echoInfo "Check ${check} changed. Queue for rebuilding."
-      changedChecks="${changedChecks:-} ${check}"
-    else
-      echoInfo "Check ${check} did not change. Not rebuilding."
-    fi
-  done
-fi
-
-echoWarn "Checks to be rebuilt are: ${changedChecks}"
-
 echoInfo "Generating pipeline..."
 
-for check in ${changedChecks}; do
+for check in ${checks}; do
 echoInfo "Generating job for check ${check}..."
 cat <<EOF
 ${check}:
