f4dfeebe44e0d1cdac9d4c5c2819ddbf896cf11a
Nicolas Froger <nico@cri.epita.fr>
2022-02-15 14:18:24 +0100

    images: nixos-pie: add podman and podman-compose
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- images/nixos-pie.nix
+++ images/nixos-pie.nix
@@ -11,6 +11,12 @@
   cri.programs.packages = with config.cri.programs.packageBundles; [ dev devSQL devThl devJava ];
   cri.programs.pythonPackages = with config.cri.programs.pythonPackageBundles; [ dev devThl ];
 
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+  };
+  environment.systemPackages = with pkgs; [ podman-compose ];
+
   cri.nswrappers.enable = true;
 
   programs.java = {
