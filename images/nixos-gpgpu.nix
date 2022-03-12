86941ce6ca5035c41e4995f77d292d8fb732c846
Charles Decoux <charles@cri.epita.fr>
2022-03-12 10:13:17 +0100

    meta: move default branch to main
    Closes #43
    
    Signed-off-by: Charles Decoux <charles@cri.epita.fr>

--- images/nixos-gpgpu.nix
+++ images/nixos-gpgpu.nix
@@ -41,7 +41,7 @@
     let
       system = "x86_64-linux";
 
-      nixpie = import (builtins.fetchTarball "https://gitlab.cri.epita.fr/cri/infrastructure/nixpie/-/archive/master/nixpie-master.tar.gz");
+      nixpie = import (builtins.fetchTarball "https://gitlab.cri.epita.fr/cri/infrastructure/nixpie/-/archive/main/nixpie-main.tar.gz");
 
       inherit (nixpie.inputs.nixpkgs) lib;
       inherit (lib) attrValues;
