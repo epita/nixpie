1fc43e9bbfeef4004b054f99f559343a7b3f118b
Nicolas Froger <nico@cri.epita.fr>
2022-04-08 09:10:47 +0200

    profiles: graphical: set firefox as default browser
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- profiles/graphical/default.nix
+++ profiles/graphical/default.nix
@@ -45,6 +45,18 @@ in
     };
   };
 
+  xdg.mime.defaultApplications = {
+    "x-scheme-handler/http" = "firefox.desktop";
+    "x-scheme-handler/https" = "firefox.desktop";
+    "text/html" = "firefox.desktop";
+    "application/x-extension-htm" = "firefox.desktop";
+    "application/x-extension-html" = "firefox.desktop";
+    "application/x-extension-shtml" = "firefox.desktop";
+    "application/xhtml+xml" = "firefox.desktop";
+    "application/x-extension-xhtml" = "firefox.desktop";
+    "application/x-extension-xht" = "firefox.desktop";
+  };
+
   fonts = {
     enableDefaultFonts = true;
     fonts = with pkgs; [
