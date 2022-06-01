900e864297b5b0e652654feb2ab0e4a652c06a69
Nicolas Froger <nico@cri.epita.fr>
2022-06-01 14:21:43 +0200

    modules: pkgs: r: add FactoMineR package
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- modules/packages/pkgs/r.nix
+++ modules/packages/pkgs/r.nix
@@ -1,5 +1,10 @@
 { config, lib, pkgs, ... }:
 
+let
+  nixpieRPackages = with pkgs.rPackages; [
+    FactoMineR
+  ];
+in
 {
   options = {
     cri.packages.pkgs.r.enable = lib.options.mkEnableOption "dev R CRI package bundle";
@@ -7,8 +12,8 @@
 
   config = lib.mkIf config.cri.packages.pkgs.r.enable {
     environment.systemPackages = with pkgs; [
-      rWrapper
-      rstudioWrapper
+      (rWrapper.override { packages = nixpieRPackages; })
+      (rstudioWrapper.override { packages = nixpieRPackages; })
     ];
   };
 }
