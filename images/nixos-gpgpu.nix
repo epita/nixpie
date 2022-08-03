f6c52702dcba6e66cbb1afbeedc48a09d604996d
Nicolas Froger <nico@cri.epita.fr>
2022-08-03 18:47:45 +0200

    images: nixos-gpgpu: fix shell.nix import and packages
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- images/nixos-gpgpu.nix
+++ images/nixos-gpgpu.nix
@@ -41,7 +41,7 @@
     let
       system = "x86_64-linux";
 
-      nixpie = import (builtins.fetchTarball "https://gitlab.cri.epita.fr/cri/infrastructure/nixpie/-/archive/master/nixpie-master.tar.gz");
+      nixpie = import <nixpie>;
 
       inherit (nixpie.inputs.nixpkgs) lib;
       inherit (lib) attrValues;
@@ -64,13 +64,9 @@
         xorg.libXi xorg.libXmu freeglut
         xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib pngpp tbb
         ncurses5 stdenv.cc binutils
-        (cudatoolkit.override {
-          gcc = stdenv.cc;
-        })
       ];
       shellHook = with pkgs;'' + "''" + ''
-    export CUDA_PATH=''${pkgs.cudatoolkit}
-    export PATH=$CUDA_PATH/nsight_compute:$CUDA_PATH/nsight_systems/host-linux-x64:$PATH
+    export CUDA_PATH=''${pkgs.cudaPackages.cudatoolkit}
     export LD_LIBRARY_PATH=''${linuxPackages.nvidia_x11}/lib:''${ncurses5}/lib:''${libkrb5}/lib:$LD_LIBRARY_PATH
     export EXTRA_LDFLAGS="-L/lib -L''${linuxPackages.nvidia_x11}/lib $EXTRA_LDFLAGS"
     export EXTRA_CCFLAGS="-I/usr/include $EXTRA_CCFLAGS"
