1672a3926b8c614f54b2ee18fa157ade8ae0db6d
Nicolas Froger <nico@cri.epita.fr>
2023-01-19 21:14:33 +0100

    pkgs: i3lock: update to 2.14
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- pkgs/applications/window-managers/i3/lock.nix
+++ pkgs/applications/window-managers/i3/lock.nix
@@ -1,31 +1,21 @@
-{ lib
-, fetchFromGitHub
-, autoreconfHook
-, systemd
-, i3lock
-}:
+{ stdenv, lib, fetchFromGitHub, i3lock, systemd }:
 
 i3lock.overrideAttrs (old: rec {
   pname = "i3lock-cri";
-  version = "2.12-cri";
+  version = "2.14.1-cri";
 
   src = fetchFromGitHub {
     owner = "epita";
     repo = "i3lock";
     rev = version;
-    sha256 = "0qk89f44bifj1hxvzqi5gl0rdi5pfnsziwzi3jm7x1v3g2r2iny7";
+    sha256 = "sha256-hybNDRmC0St4sMz0Ff98G+a4kz0Nex4itrsRKu7tSHY=";
   };
 
-  nativeBuildInputs = old.nativeBuildInputs ++ [ autoreconfHook ];
   propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [ systemd ];
 
   postPatch = ''
     sed -i -e 's:login:system-auth:' pam/i3lock
   '';
-
-  configureFlags = [ "--disable-sanitizers" ];
-
-  makeFlags = [ ''CPPFLAGS+="-U_FORTIFY_SOURCE"'' ];
 
   meta = with lib; {
     platforms = platforms.linux;
