4f0eb69bbcce3f1f87eb352df6d8c23a9cf05c93
Nicolas Fidel <nicolas.fidel@epita.fr>
2023-02-23 15:53:07 +0100

    pkgs: development: compilers: bump nolimips

--- pkgs/development/compilers/nolimips/default.nix
+++ pkgs/development/compilers/nolimips/default.nix
@@ -1,16 +1,42 @@
 { lib
-, fetchurl
+, fetchgit
 , stdenv
+, pkgs
 }:
 
 stdenv.mkDerivation rec {
   pname = "nolimips";
   version = "0.11";
 
-  src = fetchurl {
-    url = "https://www.lrde.epita.fr/~tiger/download/${pname}-${version}.tar.gz";
-    sha256 = "sha256-RPizR991vR+pvSGt1+Coy45cNIMGUgc4vKCI9xB+Bgg=";
+  src = fetchgit {
+    url = "https://gitlab.lre.epita.fr/tiger/nolimips.git";
+    rev = "d116bc9efe62aa2d86bb9f24c956a29fc68e6919";
+    sha256 = "sha256-WAZlIgApe8nLdCQ6EyJX4PAqwQzf0se8rjjeQfiPwH8=";
   };
+
+  enableParallelBuilding = true;
+
+  preConfigure = ''
+    patchShebangs --build ./dev/
+    patchShebangs --build ./doc/
+    patchShebangs --build ./src/inst/
+    patchShebangs --build ./src/parse/
+    ./bootstrap
+  '';
+
+  nativeBuildInputs = with pkgs; [
+    libtool
+    autoconf
+    autoconf-archive
+    automake
+    gnum4
+    bison
+    flex
+    clang
+    texinfo
+    python3
+    perl
+  ];
 
   doCheck = true;
   doInstallCheck = true;
@@ -19,6 +45,6 @@ stdenv.mkDerivation rec {
     description = "Nolimips, basic MIPS architecture simulator";
     homepage = "https://www.lrde.epita.fr/wiki/Nolimips";
     license = licenses.gpl2;
-    platforms = platforms.unix;
+    platforms = platforms.all;
   };
 }
