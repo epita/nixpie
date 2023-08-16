c6aabbc1e1cbce567be0de6d9be12ba23abd2456
Nicolas Froger <nico@cri.epita.fr>
2023-08-16 15:14:43 +0200

    pkgs: overrides: remove pysqlcipher3
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- pkgs/overrides.nix
+++ pkgs/overrides.nix
@@ -3,10 +3,4 @@
 final: prev: {
   inherit (pkgsUnstable)
     httplib;
-
-  python3 = prev.python3.override {
-    packageOverrides = python-self: python-super: {
-      inherit (pkgsUnstable.python3Packages) pysqlcipher3;
-    };
-  };
 }
