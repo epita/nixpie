a4401e88846f090bffcec269c80a6174eb12e139
Nicolas Froger <nico@cri.epita.fr>
2023-08-16 15:11:29 +0200

    flake: set openssl allowed insecure version to 1.1.1v
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- flake.nix
+++ flake.nix
@@ -53,7 +53,7 @@
           config = {
             allowUnfree = true;
             permittedInsecurePackages = [
-              "openssl-1.1.1u"
+              "openssl-1.1.1v"
             ];
 
           };
