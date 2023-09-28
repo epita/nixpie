173e582ac0a83c565e2133e0ce0d44107dc13d90
Jean-Baptiste Bussignies <jb@cri.epita.fr>
2023-09-28 13:21:56 +0200

    images: nixos-pie: add acu-theme-2024

--- pkgs/applications/display-managers/sddm/sddm-epita-themes.nix
+++ pkgs/applications/display-managers/sddm/sddm-epita-themes.nix
@@ -1,15 +1,15 @@
 { lib, stdenv, fetchurl, extraThemeConfig ? "" }:
 
 let
-  themes = [ "epita-simplyblack" "epita-acu-2023" ];
+  themes = [ "epita-simplyblack" "epita-acu-2023" "epita-acu-2024" ];
 in
 stdenv.mkDerivation rec {
   pname = "sddm-epita-themes";
-  version = "1.1.1";
+  version = "1.2.1";
 
   src = fetchurl {
     url = "https://gitlab.cri.epita.fr/cri/packages/epita-themes-sddm/-/archive/${version}/epita-themes-sddm-${version}.tar.gz";
-    sha256 = "8a05d60f6c29787f52aa0ad4eae08e9fd40f4bf0c6da3c2535d5272399949f22";
+    sha256 = "f05293252ddc2fa7021c122f8f3d87338c5be92604f9787b99f1fa4ef011836c";
   };
 
   unpackPhase = ''
