d3cd39879d96afe1fa9879814a1b31b9e135a74e
Nicolas Froger <nico@cri.epita.fr>
2024-01-24 16:10:57 +0100

    modules: packages: libvirt: wrap libvirt, fix scripts
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- modules/packages/pkgs/libvirt.nix
+++ modules/packages/pkgs/libvirt.nix
@@ -1,5 +1,51 @@
 { config, lib, pkgs, ... }:
 
+let
+  # The following wrappers override the XDG_CONFIG_HOME environment variable to
+  # prevent libvirt from using ~/.config for anything. The reason behind this
+  # is that ~/.config is a symlink to student's AFS volumes. We don't want to
+  # persist this config and it simply does not work because QEMU cannot create
+  # Unix sockets in AFS.
+  wrappedVirtManager = pkgs.symlinkJoin {
+    name = "virt-manager";
+    paths = [ pkgs.virt-manager ];
+    postBuild = ''
+      prog=$out/bin/virt-manager
+
+      hidden="$(dirname "$prog")/.$(basename "$prog")"-wrapped
+      while [ -e "$hidden" ]; do
+        hidden="''${hidden}_"
+      done
+
+      mv "$prog" "$hidden"
+
+      echo 'export XDG_CONFIG_HOME="$HOME/.tmpconfig"' >> "$prog"
+      echo "exec -a virt-manager $hidden" >> "$prog"
+
+      chmod +x $prog
+    '';
+  };
+  wrappedLibvirt = pkgs.symlinkJoin {
+    name = "libvirt";
+    paths = [ pkgs.libvirt ];
+    postBuild = ''
+      prog=$out/bin/virsh
+
+      hidden="$(dirname "$prog")/.$(basename "$prog")"-wrapped
+      while [ -e "$hidden" ]; do
+        hidden="''${hidden}_"
+      done
+
+      mv "$prog" "$hidden"
+
+      echo 'export XDG_CONFIG_HOME="$HOME/.tmpconfig"' >> "$prog"
+      echo "exec -a virsh $hidden" >> "$prog"
+
+      chmod +x $prog
+    '';
+  };
+in
+
 {
   options = {
     cri.packages.pkgs.libvirt = {
@@ -12,15 +58,17 @@
     {
       virtualisation.libvirtd = {
         enable = true;
+        package = wrappedLibvirt;
       };
 
       environment.systemPackages = with pkgs; [
-        virt-manager
+        wrappedVirtManager
+        aria # for iso download
       ];
 
       systemd.services.libvirtd-config.script = lib.mkAfter ''
         mkdir -p /var/lib/libvirt/qemu/networks/autostart
-        ln -s /var/lib/libvirt/qemu/networks/default.xml /var/lib/libvirt/qemu/networks/autostart/
+        ln -sf /var/lib/libvirt/qemu/networks/default.xml /var/lib/libvirt/qemu/networks/autostart/
       '';
     }
     (lib.mkIf config.cri.packages.pkgs.libvirt.enableDiskPartition {
@@ -31,7 +79,11 @@
         script = ''
           set -euo pipefail
 
-          if [ ! -f /dev/disk/by-partlabel/libvirt-workdir ]; then
+          if [ -e /run/forge-libvirt-disk-setup.done ]; then
+            exit
+          fi
+
+          if [ ! -e /dev/disk/by-partlabel/libvirt-workdir ]; then
 
             echo "Scanning disks on the system:"
             disks="$(lsblk --list --noheadings --paths --output NAME,SIZE,TYPE | grep ' disk')"
@@ -62,6 +114,8 @@
           mkdir -p /srv/libvirt-workdir
           mount /dev/disk/by-partlabel/libvirt-workdir /srv/libvirt-workdir
           chmod -R 777 /srv/libvirt-workdir
+
+          touch /run/forge-libvirt-disk-setup.done
         '';
 
         serviceConfig = {
