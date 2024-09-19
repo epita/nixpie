6a6f7859b177fdb4418c220a6498e63f80517a99
Theo Gardet <theo.gardet@epita.fr>
2024-09-19 13:04:00 +0200

    feat(images: nixos-cnix-tty): add podman and command to launch tty

--- images/nixos-cnix-tty.nix
+++ images/nixos-cnix-tty.nix
@@ -1,11 +1,32 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
+let
+  tty_launch = pkgs.writeShellScriptBin "tty_launch" ''
+    #!/bin/sh
+
+    REGISTRY=registry.cri.epita.fr/ing/assistants/subjects/piscine/exercises-misc/tty:latest
+    CREDS="--authfile=/home/theo.gardet/afs/auth.json" # FIXME: This should not go into production
+    # We systematically clean the remnants of the previous image to avoid the
+    # command failing because of a lack of space on the device.
+    # FIXME: This still creates a lot of <none> images.
+    podman stop --all
+    podman container rm --all
+    podman rmi "$REGISTRY"
+    podman pull $CREDS "$REGISTRY"
+    podman run -it $CREDS "$REGISTRY"
+  '';
+in
 {
-  imports = [ ];
-
   # Define the shell script to generate the static /etc/issue file
   environment.etc."issue".text = lib.strings.concatStrings [
     (builtins.readFile ./tty-issue)
     "\n${config.system.nixos.distroName} ${config.system.nixos.label} (\\m) - \\l\n\n"
   ];
+
+  cri.packages.pkgs.podman.enable = true;
+
+  environment.systemPackages = [
+    tty_launch
+  ];
 }
+
