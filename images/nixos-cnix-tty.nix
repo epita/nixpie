f3e39774c0fe35512e4fcb0bc30adecbb6ad1930
Theo Gardet <theo.gardet@epita.fr>
2024-09-24 21:56:16 +0200

    feat(images: nixos-cnix-tty): WIP fetching image with torrenting

--- images/nixos-cnix-tty.nix
+++ images/nixos-cnix-tty.nix
@@ -1,31 +1,76 @@
 { config, lib, pkgs, ... }:
 
 let
-  tty_launch = pkgs.writeShellScriptBin "tty_launch" ''
-    #!/bin/sh
-
-    # TAG=second-crashtest-friday
-    TAG=latest
-    REGISTRY=registry.cri.epita.fr/ing/assistants/subjects/piscine/exercises-misc/tty:$TAG
-    CREDS="--authfile=$HOME/afs/auth.json" # FIXME: This should not go into production
-    # We systematically clean the remnants of the previous image to avoid the
-    # command failing because of a lack of space on the device.
-    # FIXME: This still creates a lot of <none> images.
-    podman stop --all
-    podman container rm --all
-    podman rmi "$REGISTRY"
-    podman pull $CREDS "$REGISTRY"
+  run_image = pkgs.writeShellScriptBin "run_image" ''
+    IMAGE_NAME=$1
+    LATEST_TAG=$2
 
     # Hack to be able to chown those files in the container
     cat $HOME/afs/.confs/gitconfig > $HOME/tty_gitconfig 
-    mkdir $HOME/tty_ssh
+    ! [ -d $HOME/tty_sh ] && mkdir $HOME/tty_ssh
     for f in $HOME/afs/.confs/ssh/*; do
       name=$(basename "$f")
       cat $f > $HOME/tty_ssh/$name
     done
 
+    # Run the container
     KRB5CCACHE=$(klist | head -1 | cut -d : -f 3)
-    podman run -it -v $KRB5CCACHE:/tmp/krb5cc_1000 -v $HOME/tty_gitconfig:/home/student/.gitconfig:copy,U -v $HOME/tty_ssh:/home/student/.ssh/:copy,U $CREDS "$REGISTRY"
+    podman run -it -v $KRB5CCACHE:/tmp/krb5cc_1000                              \
+                   -v $HOME/tty_gitconfig:/home/student/.gitconfig:copy,U       \
+                   -v $HOME/tty_ssh:/home/student/.ssh/:copy,U                  \
+                   $IMAGE_NAME:$LATEST_TAG
+  '';
+
+  aria2c_hook = pkgs.writeShellScriptBin "aria2c_hook" ''
+    set -ux
+
+    GID=$1
+    FILE_NUMBER=$2
+    FILE_PATH=$3
+
+    IMAGE_NAME=tty-env
+
+    podman image import "$FILE_PATH" "$IMAGE_NAME"
+  
+    LATEST_TAG=$(curl $LATEST_TAG_URL)
+    run_image "$IMAGE_NAME" "$LATEST_TAG"
+  '';
+
+  tty_launch = pkgs.writeShellScriptBin "tty_launch" ''
+    #!/bin/sh
+
+    set -ux
+
+    IMAGE_NAME=tty-env
+
+    IMAGE_TAR_NAME=$IMAGE_NAME.tar.gz
+    TORRENT_NAME=$IMAGE_TAR_NAME.torrent
+
+    S3_BUCKET=https://s3.cri.epita.fr/acu-cnix-tty-activity
+    LATEST_TAG_URL=$S3_BUCKET/latest_tag
+    TORRENT_URL=$S3_BUCKET/$TORRENT_NAME
+
+    LATEST_TAG=$(curl $LATEST_TAG_URL)
+
+    # If the image with the fetched tag is absent, download the torrent file
+    podman image exists $IMAGE_NAME:$LATEST_TAG > /dev/null 2>&1 
+    if [ $? -ne 0 ]; then
+      # ! [ -d /srv/torrent/ ] && mkdir /srv/torrent
+      # cd /srv/torrent
+
+      curl $TORRENT_URL --output $TORRENT_NAME
+      ${pkgs.aria2}/bin/aria2c --on-download-complete aria2c_hook \
+                               --enable-dht=false       \
+                               --enable-dht6=false      \
+                               --seed-ratio=0           \
+                               "$TORRENT_NAME"&
+    else
+      run_image "$IMAGE_NAME" "$LATEST_TAG"
+    fi
+    # TODO:
+    # Make sure to update /srv/torrent/aria2_seedlist.txt to include your file. 
+    # Make sure you don't mess with the rest of the file as it is used to seed PIE squashfs. 
+    # Maybe systemctl restart aria2, I don't remember if the file is hot reloaded.
   '';
 in
 {
@@ -35,10 +80,18 @@ in
     "\n${config.system.nixos.distroName} ${config.system.nixos.label} (\\m) - \\l\n\n"
   ];
 
-  cri.packages.pkgs.podman.enable = true;
+  cri = {
+    packages.pkgs.podman.enable = true;
+
+    aria2.enable = true;
+  };
+
+  netboot.enable = true;
 
   environment.systemPackages = [
     tty_launch
+    run_image
+    aria2c_hook
   ];
 }
 
