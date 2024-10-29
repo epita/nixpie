37dd869fefce38623f1806634f6307e5cc2908e0
Nicolas Froger <nico@cri.epita.fr>
2024-10-29 16:37:53 +0100

    modules: pkgs: csharp: add dotnet documentation pdf
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- modules/packages/pkgs/csharp.nix
+++ modules/packages/pkgs/csharp.nix
@@ -1,5 +1,15 @@
 { config, lib, pkgs, ... }:
 
+let
+  dotnetDocumentationPdf = pkgs.fetchurl {
+    url = "https://learn.microsoft.com/pdf?url=https%3A%2F%2Flearn.microsoft.com%2Ffr-fr%2Fdotnet%2Fapi%2F_splitted%2Fsystem%2Ftoc.json%3Fview%3Dnet-7.0";
+    hash = "sha256-xQenB2Vwync8oy1hbYtaNS4iuUcdwD7utBfn/v82Yf0=";
+  };
+  dotnetDocumentationOpen = pkgs.writeScriptBin "dotnet-documentation" ''
+    echo "Opening Dotnet documentation..."
+    ${pkgs.xdg-utils}/bin/xdg-open ${dotnetDocumentationPdf} >/dev/null 2>/dev/null </dev/null & disown
+  '';
+in
 {
   options = {
     cri.packages.pkgs.csharp.enable = lib.options.mkEnableOption "dev C# CRI package bundle";
@@ -16,6 +26,7 @@
       jetbrains.rider
       mono
       msbuild
+      dotnetDocumentationOpen
     ];
 
     environment.variables = {
