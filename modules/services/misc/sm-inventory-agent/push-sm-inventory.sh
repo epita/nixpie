e5dc7da36a5a959e24a02bb11d4ac7b93c4c39b3
Nicolas Froger <nico@cri.epita.fr>
2024-12-13 11:21:10 +0100

    modules: services: sm-inventory-agent: fallback on dmi decode type 2 when type 1 is incomplete
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- modules/services/misc/sm-inventory-agent/push-sm-inventory.sh
+++ modules/services/misc/sm-inventory-agent/push-sm-inventory.sh
@@ -11,15 +11,36 @@ done
 DISPLAY_VENDOR=$(echo "$EDID" | grep VendorName | sed -E 's/^\s+[A-Za-z]+ "(.+)"$/\1/')
 DISPLAY_MODEL=$(echo "$EDID" | grep ModelName | sed -E 's/^\s+[A-Za-z]+ "(.+)"$/\1/')
 
+COMPUTER_VENDOR=$(echo "$DMIINFO" | grep "Manufacturer" | sed -E 's/^\s+[^:]+:\s+//')
+COMPUTER_MODEL=$(echo "$DMIINFO" | grep "Product Name" | sed -E 's/^\s+[^:]+:\s+//')
+COMPUTER_VERSION=$(echo "$DMIINFO" | grep "Version" | sed -E 's/^\s+[^:]+:\s+//')
+COMPUTER_SKU=$(echo "$DMIINFO" | grep "SKU Number" | sed -E 's/^\s+[^:]+:\s+//')
+COMPUTER_SERIAL_NUMBER=$(echo "$DMIINFO" | grep "Serial Number" | sed -E 's/^\s+[^:]+:\s+//')
+
+DMIINFO=$(dmidecode -t 2)
+
+if [ -z "$COMPUTER_VENDOR" ]; then
+    COMPUTER_VENDOR=$(echo "$DMIINFO" | grep "Manufacturer" | sed -E 's/^\s+[^:]+:\s+//')
+fi
+if [ -z "$COMPUTER_MODEL" ]; then
+    COMPUTER_MODEL=$(echo "$DMIINFO" | grep "Product Name" | sed -E 's/^\s+[^:]+:\s+//')
+fi
+if [ -z "$COMPUTER_VERSION" ]; then
+    COMPUTER_VERSION=$(echo "$DMIINFO" | grep "Version" | sed -E 's/^\s+[^:]+:\s+//')
+fi
+if [ -z "$COMPUTER_SERIAL_NUMBER" ]; then
+    COMPUTER_SERIAL_NUMBER=$(echo "$DMIINFO" | grep "Serial Number" | sed -E 's/^\s+[^:]+:\s+//')
+fi
+
 PAYLOAD=$(cat <<EOF
 {
   "fqdn": "$(hostname -f)",
   "computer": {
-    "vendor": "$(echo "$DMIINFO" | grep "Manufacturer" | sed -E 's/^\s+[^:]+:\s+//')",
-    "model": "$(echo "$DMIINFO" | grep "Product Name" | sed -E 's/^\s+[^:]+:\s+//')",
-    "version": "$(echo "$DMIINFO" | grep "Version" | sed -E 's/^\s+[^:]+:\s+//')",
-    "sku": "$(echo "$DMIINFO" | grep "SKU Number" | sed -E 's/^\s+[^:]+:\s+//')",
-    "serialNumber": "$(echo "$DMIINFO" | grep "Serial Number" | sed -E 's/^\s+[^:]+:\s+//')"
+    "vendor": "$COMPUTER_VENDOR",
+    "model": "$COMPUTER_MODEL",
+    "version": "$COMPUTER_VERSION",
+    "sku": "$COMPUTER_SKU",
+    "serialNumber": "$COMPUTER_SERIAL_NUMBER"
   },
   "display": {
     "vendor": "$DISPLAY_VENDOR",
