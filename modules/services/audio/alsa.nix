6836ce40d2ea7efa0bac8b48b6a2cc44a3fce7b7
Nicolas Froger <nico@cri.epita.fr>
2024-12-17 12:08:37 +0100

    modules: sound: replace pulseaudio with pipewire
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- modules/services/audio/alsa.nix
+++ modules/services/audio/alsa.nix
@@ -10,13 +10,11 @@ with lib;
   };
 
   config = mkIf config.cri.sound.enable {
-    sound.enable = true;
-
-    hardware = {
-      pulseaudio = {
-        enable = true;
-        package = mkIf config.cri.bluetooth.enable pkgs.pulseaudioFull;
-      };
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
     };
 
     environment.systemPackages = with pkgs; [
