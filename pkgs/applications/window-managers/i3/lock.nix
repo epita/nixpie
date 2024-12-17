64fc003c63d72f8b40bc09a871e322a04c3ae685
Nicolas Froger <nico@cri.epita.fr>
2024-12-17 12:13:44 +0100

    pkgs: i3lock: remove custom repo, use patch instead
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- pkgs/applications/window-managers/i3/lock.nix
+++ pkgs/applications/window-managers/i3/lock.nix
@@ -1,17 +1,11 @@
-{ stdenv, lib, fetchFromGitHub, i3lock, systemd }:
+{ stdenv, lib, i3lock, systemd }:
 
 i3lock.overrideAttrs (old: rec {
-  pname = "i3lock-cri";
-  version = "2.14.1-cri";
-
-  src = fetchFromGitHub {
-    owner = "epita";
-    repo = "i3lock";
-    rev = version;
-    sha256 = "sha256-hybNDRmC0St4sMz0Ff98G+a4kz0Nex4itrsRKu7tSHY=";
-  };
-
   propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [ systemd ];
+
+  patches = [
+    ./i3lock-epita.patch
+  ];
 
   postPatch = ''
     sed -i -e 's:login:system-auth:' pam/i3lock
