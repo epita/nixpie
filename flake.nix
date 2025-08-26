9768aa25930640772b348a77a4c93d2d7ff42c95
Nicolas Froger <nico@cri.epita.fr>
2025-08-26 08:53:08 +0200

    flake: update inputs to 25.05
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   '';
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgsUnstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgsMaster.url = "github:NixOS/nixpkgs/master";
 
