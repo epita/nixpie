989d42c8749f85b12375ecb3246abe0a9dd6cde9
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-09-25 11:07:46 +0200

    pkgs: sddm: add acu-theme-2026 theme
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- pkgs/applications/display-managers/sddm/sddm-epita-themes.nix
+++ pkgs/applications/display-managers/sddm/sddm-epita-themes.nix
@@ -1,15 +1,15 @@
 { lib, stdenv, fetchurl, extraThemeConfig ? "" }:
 
 let
-  themes = [ "epita-simplyblack" "epita-acu-2023" "epita-acu-2024" "epita-acu-2025" ];
+  themes = [ "epita-simplyblack" "epita-acu-2023" "epita-acu-2024" "epita-acu-2025" "epita-acu-2026" ];
 in
 stdenv.mkDerivation rec {
   pname = "sddm-epita-themes";
-  version = "1.3.1";
+  version = "1.4.0";
 
   src = fetchurl {
     url = "https://gitlab.cri.epita.fr/forge/packages/epita-themes-sddm/-/archive/${version}/epita-themes-sddm-${version}.tar.gz";
-    sha256 = "48f202720be5f2bd101e94e8e868a5180ffe9930b90f5cce5a19d20f4b22c2d6";
+    sha256 = "sha256-ffadjlkMsjf0vmTiWq05qKQcKC9I37XVKiSz38YxujE=";
   };
 
   unpackPhase = ''
