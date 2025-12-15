577696bdeb1df5ddd2e331eaa5b879eb8b62df4a
Nicolas Froger <nico@cri.epita.fr>
2025-12-15 15:07:48 +0100

    modules: pkgs: rust: add rustfmt and clippy
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- modules/packages/pkgs/rust.nix
+++ modules/packages/pkgs/rust.nix
@@ -9,6 +9,8 @@
     environment.systemPackages = with pkgs; [
       cargo
       rustc
+      rustfmt # FORGE #66509
+      clippy # FORGE #66509
       (writeScriptBin "rust-doc" ''
         echo "Opening Rust documentation..."
         ${xdg-utils}/bin/xdg-open ${rustc.doc}/share/doc/docs/html/index.html >/dev/null 2>/dev/null </dev/null & disown
