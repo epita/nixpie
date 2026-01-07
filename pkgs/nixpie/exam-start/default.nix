68006fbf8676035e51bc8432eba6023ba7a47b75
Nicolas Froger <nico@cri.epita.fr>
2026-01-07 22:33:54 +0000

    pkgs: nixpie: exam-start: fix python package

--- pkgs/nixpie/exam-start/default.nix
+++ pkgs/nixpie/exam-start/default.nix
@@ -7,11 +7,14 @@ let
   requests-gssapi = python3Packages.buildPythonPackage rec {
     pname = "requests-gssapi";
     version = "1.2.3";
+    pyproject = true;
 
     src = python3Packages.fetchPypi {
       inherit pname version;
       sha256 = "sha256-IHhFCJgUAfcVPJM+7QlTOJM6QIGNplolnb8tgNzLFQ4=";
     };
+
+    build-system = [ python3Packages.setuptools ];
 
     propagatedBuildInputs = with python3Packages; [
       gssapi
