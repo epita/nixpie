fd25e9d3f38b2f5e3d689e8e1dc26b7bfede0109
Nicolas Froger <nico@cri.epita.fr>
2026-03-07 16:56:02 +0100

    hotfix for java exam 09/03/2026
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- images/exam-pie.nix
+++ images/exam-pie.nix
@@ -1,7 +1,26 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 with lib;
 
+let
+  m2Dir = pkgs.stdenv.mkDerivation {
+    pname = "ing-ing1-java-exam-m2";
+    version = "20260307";
+    src = pkgs.fetchurl {
+      url = "https://s3.cri.epita.fr/cri-nico-uploads/ing-ing1-java-exam-maven.tar.xz";
+      sha256 = "sha256-vWVNAm9eD4wAZK5Fn0Gj8Vk7U6Ay2HAihsM5PYdd9Ag=";
+    };
+
+    unpackPhase = ''
+      tar xvf $src
+    '';
+
+    installPhase = ''
+      mkdir -p $out/.m2
+      cp -v -r repository settings.xml $out/.m2
+    '';
+  };
+in
 {
   imports = [
     ../profiles/graphical
@@ -19,4 +38,6 @@ with lib;
   };
 
   cri.sddm.title = lib.mkForce "Exam PIE";
+
+  security.pam.makeHomeDir.skelDirectory = m2Dir.outPath;
 }
