0fbd8b6278586fe24260803b21fa2b75dfec08d5
Nicolas Froger <nico@cri.epita.fr>
2026-06-29 20:19:53 +0200

    modules: pkgs: cpp: add vscode with extensions
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- modules/packages/pkgs/cpp.nix
+++ modules/packages/pkgs/cpp.nix
@@ -11,5 +11,12 @@
       yaml-cpp
       jetbrains.clion # FIXME: only for exam in 2526, remove me after
     ];
+
+    cri.packages.pkgs.codium.enable = true;
+    cri.packages.pkgs.codium.extensions = with pkgs.vscode-extensions; [
+      ms-vscode.cpptools
+      ms-vscode.cmake-tools
+      vscodevim.vim
+    ];
   };
 }
