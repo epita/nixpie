5ba6f21deeb5e599ce67ecb192ce0b800f4fe740
Nicolas Froger <nico@cri.epita.fr>
2026-08-18 12:05:39 +0200

    images: nixos-net: remove cisco packet tracer
    Signed-off-by: Nicolas Froger <nico@cri.epita.fr>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -116,10 +116,10 @@
     args = final: prev: { inherit (prev) i3lock; };
   };
 
-  ciscoPacketTracer8 = {
-    path = ../applications/networking/cisco-packet-tracer;
-    args = final: prev: { inherit (prev) ciscoPacketTracer8; };
-  };
+  # ciscoPacketTracer9 = {
+  #   path = ../applications/networking/cisco-packet-tracer;
+  #   args = final: prev: { inherit (prev) ciscoPacketTracer9; };
+  # };
 
   tina = ../applications/editors/tina;
 
